c9bc1488d4adec7ae3316fab4b505dad5b3a9d09
michel.vvs <michel.vvs@gmail.com>
2021-06-23 23:38:40 -0300

    add comentários

--- src/App.js
+++ src/App.js
@@ -1,45 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import { Component, useDebugValue } from 'react';
 
 class App extends Component {
   state = {
-      name: 'totero sampla',
-      counter: 0
+      posts: []
 
     };
-  
 
-  handlePClick = () => {
-    this.setState({name: 'cobaia louca'});
+  timeoutUpdate = null;
+  
+  componentDidMount() {
+      this.loadPosts();
   }
 
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState ({counter: counter + 1} )
-    console.log (counter)
+
+    loadPosts = async () => {
+      const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+      const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+      const commentsResponse = fetch('https://jsonplaceholder.typicode.com/comments');
+      const [posts, photos, comments] = await Promise.all([postsResponse, photosResponse, commentsResponse]);
+      const postsJson = await posts.json();
+      const photosJson = await photos.json();
+      const commentsJson = await comments.json();
+      const postAndPhotos = postsJson.map((post, index) => {
+        const commentsOfpost = commentsJson.filter(comment => ( comment.postId === index));
+        return { ...post, cover: photosJson[index].url, 
+          comments: commentsOfpost
+        }
+      });
+      this.setState({ posts: postAndPhotos})
+
+    }
+
+
+  handleTimeOut = () => {
+    const { posts } = this.state;
+    posts[0].title = 'novo titulo'
+    
+
+
   }
 
   render () {
-    const { name, counter } = this.state;
-    return(
-      <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         <p onClick={this.handlePClick }>
-           bola cheia é o {name} 
-         </p>
-         <a onClick={this.handleAClick}
-           className="App-link"
-           href="https://reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-         >
-           link loko ({counter})
-         </a>
-       </header>
-     </div>
+    const { posts } = this.state;
+    return (
+      <section className="container">
+          <div className="posts"> 
+          
+          {posts.map(post => (
+            <div className ="post">
+              <div key={post.id} className="post-content">
+                <img src={post.cover} alt={post.title}></img>
+                <h1 >{post.title} </h1>
+                <p>{post.body}</p>
+                <div className="comments">
+                  <p><strong>comentários</strong></p>
+                    {post.comments.map(comment => (
+                      <p>nome: {comment.name}</p>
+                    ))}
+                  
+                </div>
+              </div>
+            </div> 
+          )) }
+        </div>
+      </section>
       
     )
   }
